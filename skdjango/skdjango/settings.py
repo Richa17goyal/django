d545c9c86c204044b933ebda5f5308c224021fa7
Richa17goyal <richa175goyal@gmail.com>
2024-08-22 15:43:28 +0100

    adding tailwind

--- skdjango/skdjango/settings.py
+++ skdjango/skdjango/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rsProj',
+    'tailwind',
+    'theme',
 ]
+ 
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = ['127.0.0.1']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -124,3 +129,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
+
