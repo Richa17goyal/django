eb348004b8a6d8434a6c8e00b2108a0efd404740
Richa17goyal <richa175goyal@gmail.com>
2024-08-27 12:33:15 +0100

    forms

--- skdjango/rsProj/views.py
+++ skdjango/rsProj/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .models import ChaiVariety
+from .models import ChaiVariety, Store
 from django.shortcuts import get_object_or_404
+from .forms import ChaiVarietyForm
 
 # Create your views here.
 def all_chai(request):
@@ -14,5 +15,15 @@ def chai_detail(request, chai_id):
     return render(request, 'rsProj/chai_detail.html', {'chai':chai})
 
 def chai_store_view(request):
-    return render(request, 'rsProj/chai_stores.html')
+    stores = None
+    if request.method == 'POST':
+        form = ChaiVarietyForm(request.POST)
+        if form.is_valid():
+            chai_variety = form.cleaned_data['chai_variety']
+            stores = Store.objects.filter(chai_varieties=chai_variety)
+    else:
+        form = ChaiVarietyForm()
+    return render(request, 'rsProj/chai_stores.html', {'stores': stores, 'form': form})
+
+
 
